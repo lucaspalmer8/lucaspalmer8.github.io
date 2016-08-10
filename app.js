c60a60fe1a594e0b66a84c35dfaa082309ec5172
lucaspalmer8 <lucaspalmer8@gmail.com>
2016-08-10 19:12:49 -0400

    Animation

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var load = function() {
 	var details = $("#personal-details");
 	//details.removeClass("hide-details", {duration:2000});
 	//	details.addClass("show-details");
-	details.switchClass("hide-details", "show-details", 2000);
+	details.hide();
+	details.slideDown(2000);
+	//details.switchClass("hide-details", "show-details", 2000);
 };
 
 $(window).on("load", load)
