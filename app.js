b9b7dbe94557bd98e421dfa5d260567bd193591d
lucaspalmer8 <lucaspalmer8@gmail.com>
2016-08-10 22:52:01 -0400

    Menu Icon

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ var main = function() {
 	var menuIcon = $("#menu-icon");
 	var menuBar = $("#menu-bar");
 
-	menuIcon.click(function() {
+	/*menuIcon.click(function() {
 		if (menuBar.width() == 0) {
 			menuBar.animate({width: "150px"}, 1000);
 		}
 		else {
 			menuBar.animate({width: "0px"}, 1000);
 		}
-	});
+	});*/
+
+	var hoverOver = function() {
+		menuBar.animate({height: "200px"}, 500);
+	}
+	
+	var hoverOut = function() {
+		menuBar.animate({height: "0px"}, 500);
+	}
+
+	menuIcon.hover(hoverOver, hoverOut);
 	
 };
 
