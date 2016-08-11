2553fea70b2a2af9f6181b1c26781cc5ebde5706
lucaspalmer8 <lucaspalmer8@gmail.com>
2016-08-10 20:30:36 -0400

    Menu Icon

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var main = function() {
-	var details = $("#personal-details");
-	//details.hide();
+	var menuIcon = $("#menu-icon");
+	var menuBar = $("#menu-bar");
+
+	menuIcon.onClick(function() {
+		menuBar.animate({width: 140px}, 2000);
+	});
+	
 };
 
 var load = function() {
 	var details = $("#personal-details");
-	//details.removeClass("hide-details", {duration:2000});
-	//	details.addClass("show-details");
-	//details.hide();
 	details.slideDown(2000);
-	//details.switchClass("hide-details", "show-details", 2000);
 };
 
 $(window).on("load", load)
