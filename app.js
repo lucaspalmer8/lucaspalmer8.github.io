6b7457c0c2ef9bb56ea2f209dab502e134eddaff
Lucas Palmer <lucaspalmer8@gmail.com>
2017-11-12 01:39:06 -0500

    Updating menu bar, internships, and background images.

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 var main = function() {
-	var menuIcon = $("#menu-icon");
-	var menuBar = $("#menu-bar");
-	var menu = $("#menu");
-
-	var hoverOver = function() {
-		menuBar.stop();
-		menuBar.animate({height: "315px"}, 500); /* 45px for each menu item */
-	}
-	
-	var hoverOut = function() {
-		//to avoid build up in animation queue
-		menuBar.stop();
-		menuBar.animate({height: "0px"}, 500);
-	}
-
-	menu.hover(hoverOver, hoverOut);
 };
 
 var load = function() {
-	var details = $("#personal-details");
-	details.slideDown(2000);
 };
 
 $(window).on("load", load);
